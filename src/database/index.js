c89224d3048a8db9979f7d855fa01c7819c8f9ff
Gautier de MAUROY <89637837+GautierDeMo@users.noreply.github.com>
2026-04-16 11:55:25 +0200

    ✨ feat(database): implement indexedDB for markdown files
    📁 index.js: create object store for markdown files and index by date

---
+++ src/database/index.js
@@ -0,0 +1,15 @@
+import { openDB } from 'idb'
+
+export const database = await openDB('MarkdownInterface', 1, {
+  upgrade(db) {
+    // Create a store of md files
+    const markdownStore = db.createObjectStore('files', {
+      // The 'id' property of the object will be the key.
+      keyPath: 'id',
+      // If it isn't explicitly set, create a value by auto incrementing.
+      autoIncrement: true,
+    })
+    // Create an index on the 'date' property of the objects.
+    markdownStore.createIndex('date', 'date')
+  },
+})
