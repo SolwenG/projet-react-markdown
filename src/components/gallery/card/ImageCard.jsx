f80f218fe9274db5f9d2a5845d094b69695f8807
Mickael-Desclaux <mickaeldesclaux@gmail.com>
2026-04-16 11:07:36 +0200

    add fake data and props to ImageCard component

--- src/components/gallery/card/ImageCard.jsx
+++ src/components/gallery/card/ImageCard.jsx
@@ -1,12 +1,8 @@
-export default function ImageCard() {
+export default function ImageCard({ name, src }) {
   return (
     <div className="flex flex-col justify-center gap-1 items-center cursor-pointer shadow-sm rounded-lg">
-      <img
-        src="https://docs.material-tailwind.com/img/face-2.jpg"
-        alt="avatar"
-        className="rounded-t-lg max-w-3xs"
-      />
-      <p className="p-1">MyImageName.jpg</p>
+      <img src={src} alt="avatar" className="rounded-t-lg max-w-3xs" />
+      <p className="p-1">{name}</p>
     </div>
   )
 }
--- src/components/gallery/list/ImageList.jsx
+++ src/components/gallery/list/ImageList.jsx
@@ -1,37 +1,50 @@
 import ImageCard from '../card/ImageCard'
 
+const FAKE_IMAGES = [
+  {
+    id: 1,
+    name: 'myImage1.jpg',
+    src: 'https://docs.material-tailwind.com/img/face-1.jpg',
+  },
+  {
+    id: 2,
+    name: 'myImage2.jpg',
+    src: 'https://docs.material-tailwind.com/img/face-2.jpg',
+  },
+  {
+    id: 3,
+    name: 'myImage3.jpg',
+    src: 'https://docs.material-tailwind.com/img/face-3.jpg',
+  },
+  {
+    id: 4,
+    name: 'myImage4.jpg',
+    src: 'https://docs.material-tailwind.com/img/face-1.jpg',
+  },
+  {
+    id: 5,
+    name: 'myImage5.jpg',
+    src: 'https://docs.material-tailwind.com/img/face-2.jpg',
+  },
+  {
+    id: 6,
+    name: 'myImage6.jpg',
+    src: 'https://docs.material-tailwind.com/img/face-2.jpg',
+  },
+]
+
 export default function List() {
   return (
     <div>
       <h1>Hello</h1>
       <ul className="flex items-center gap-4 flex-wrap max-w-[60vw] justify-center mx-auto">
-        <li>
-          <ImageCard />
-        </li>
-        <li>
-          <ImageCard />
-        </li>
-        <li>
-          <ImageCard />
-        </li>
-        <li>
-          <ImageCard />
-        </li>
-        <li>
-          <ImageCard />
-        </li>
-        <li>
-          <ImageCard />
-        </li>
-        <li>
-          <ImageCard />
-        </li>
-        <li>
-          <ImageCard />
-        </li>
-        <li>
-          <ImageCard />
-        </li>
+        {FAKE_IMAGES.map((image) => {
+          return (
+            <li>
+              <ImageCard name={image.name} src={image.src} />
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
